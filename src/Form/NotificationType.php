482c30dd6cd375dc6c581bd034f1bdc8feff616e
Piotr Nowak <ppp.nnn.shop+42394@gmail.com>
2021-08-06 10:56:27 +0200

    NotificationType form configured to show working and incorrect scenarios

--- src/Form/NotificationType.php
+++ src/Form/NotificationType.php
@@ -3,17 +3,55 @@
 namespace App\Form;
 
 use App\Entity\Notification;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class NotificationType extends AbstractType
 {
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+
+        // when editing existing entity this will not show selected options
         $builder
-            ->add('subscribedUsers')
+            ->add('subscribedUsers', EntityType::class, [
+                'class' => User::class,
+                'choice_value' => 'id',
+                'choice_label' => function(User $u) { return $u->getName(); },
+                'multiple' => true,
+            ])
         ;
+
+//        // this works correctly
+//        $builder
+//            ->add('subscribedUsers', EntityType::class, [
+//                'class' => User::class,
+//                'choice_label' => function(User $u) { return $u->getName(); },
+//                'multiple' => true,
+//            ])
+//        ;
+
+
+//        // this also works correctly
+//        $builder
+//            ->add('subscribedUsers', EntityType::class, [
+//                'class' => User::class,
+//                'choice_value' => 'id',
+//                'choice_label' => function(User $u) { return $u->getName(); },
+//                'multiple' => true,
+//                'choices' => $this->entityManager->getRepository(User::class)->findAll(),
+//            ])
+//        ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
